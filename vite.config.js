8b6c9ad37667a0ed419b47781f70e43607dd8b7e
M.Wolyniec <mateusz.wolyniec.external@worldline.com>
2025-04-17 19:03:20 +0200

    config git deplo

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          three: ['three']
+        }
+      }
+    }
+  },
   server: {
     open: true
   }
